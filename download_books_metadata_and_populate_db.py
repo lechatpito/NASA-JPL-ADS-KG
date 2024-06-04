72228ab46c90d0c9277560675836a40f76d3fe8a
François Scharffe <lechatpito@users.noreply.github.com>
2024-06-04 12:39:34 -0400

    minor change

--- download_books_metadata_and_populate_db.py
+++ download_books_metadata_and_populate_db.py
@@ -73,28 +73,27 @@ for url in urls:
         # Parse the line as a JSON object
         book = json.loads(line)
 
-        # Convert each book entry to a SQL statement and load it to the database
-        for book in data:
-            cur.execute("""
-            INSERT INTO books (bibcode, author, bibstem, doctype, doi, id, identifier, pub, pubdate, title, institution, keyword_schema, keyword, keyword_norm, abstract)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            ON CONFLICT (bibcode) DO UPDATE SET
-            author = EXCLUDED.author,
-            bibstem = EXCLUDED.bibstem,
-            doctype = EXCLUDED.doctype,
-            doi = EXCLUDED.doi,
-            id = EXCLUDED.id,
-            identifier = EXCLUDED.identifier,
-            pub = EXCLUDED.pub,
-            pubdate = EXCLUDED.pubdate,
-            title = EXCLUDED.title,
-            institution = EXCLUDED.institution,
-            keyword_schema = EXCLUDED.keyword_schema,
-            keyword = EXCLUDED.keyword,
-            keyword_norm = EXCLUDED.keyword_norm,
-            abstract = EXCLUDED.abstract;
-            """, (book['bibcode'], book['author'], book['bibstem'], book['doctype'], book['doi'], book['id'], book['identifier'], book['pub'], book['pubdate'], book['title'], book['institution'], book['keyword_schema'], book['keyword'], book['keyword_norm'], book['abstract']))
-
+        # Execute the SQL statement
+        cur.execute("""
+        INSERT INTO books (bibcode, author, bibstem, doctype, doi, id, identifier, pub, pubdate, title, institution, keyword_schema, keyword, keyword_norm, abstract)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (bibcode) DO UPDATE SET
+        author = EXCLUDED.author,
+        bibstem = EXCLUDED.bibstem,
+        doctype = EXCLUDED.doctype,
+        doi = EXCLUDED.doi,
+        id = EXCLUDED.id,
+        identifier = EXCLUDED.identifier,
+        pub = EXCLUDED.pub,
+        pubdate = EXCLUDED.pubdate,
+        title = EXCLUDED.title,
+        institution = EXCLUDED.institution,
+        keyword_schema = EXCLUDED.keyword_schema,
+        keyword = EXCLUDED.keyword,
+        keyword_norm = EXCLUDED.keyword_norm,
+        abstract = EXCLUDED.abstract;
+        """, (book['bibcode'], book['author'], book['bibstem'], book['doctype'], book['doi'], book['id'], book['identifier'], book['pub'], book['pubdate'], book['title'], book['institution'], book['keyword_schema'], book['keyword'], book['keyword_norm'], book['abstract']))
+    
     # Commit the changes
     conn.commit()
 
